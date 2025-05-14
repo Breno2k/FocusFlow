598b2a3e247795b0c415459c5530c8ee235305f1
Breno Melo <brenolucasmelo@gmail.com>
2025-05-14 13:12:17 -0300

    feat: adicionando minutos e segundos

--- src/pages/TimerFoco.jsx
+++ src/pages/TimerFoco.jsx
@@ -1,9 +1,47 @@
-
+import { useState } from "react";
 
 const TimerFoco = () => {
-    return (
-        <div>Timer Foco</div>
-    )
-}
 
-export default TimerFoco
+    const [minutes, setMinutes] = useState(10);
+    const [seconds, setSeconds] = useState(5);
+
+    const iniciar = () => {
+
+
+
+        console.log("Timer iniciado!")
+
+        // let minutos = 25
+
+        const temporizador = setInterval(() => {
+
+            setSeconds((prevSeconds) => {
+                if (prevSeconds > 0) {
+                    return prevSeconds - 1
+                } else {
+                    console.log("Decrementando minuto!")
+                    setMinutes((prevMinutes) => {
+                        if (prevMinutes === 0) {
+                            clearInterval(temporizador);
+                            return 0
+                        };
+                        return prevMinutes - 1
+                    })
+                    return 5;
+                }
+            })
+        }, 1000)
+
+
+    }
+
+
+    return (
+        <div>
+            <button onClick={iniciar}>click aqui</button>
+            <p>{minutes}:{seconds}</p>
+        </div>
+    );
+};
+
+export default TimerFoco;
